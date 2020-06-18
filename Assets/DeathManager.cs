16059678ec925d4efb094df1e7b60a06114422a2
stenterwan <sten.terwan@student.hu.nl>
2020-06-18 17:30:23 +0200

    hele game in nieuwe git

---
+++ Assets/DeathManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathManager : MonoBehaviour
+{
+    public int deaths = 0;
+    public Text deathText;
+
+    public void IncreaseDeaths()
+    {
+        deaths += 1;
+        deathText.text = "Deaths: " + deaths.ToString();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int tokenScore = 0;
+
+    public Text scoreText;
+
+    public void IncreaseScore()
+    {
+        tokenScore += 10;
+        scoreText.text = "Score: " + tokenScore.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
